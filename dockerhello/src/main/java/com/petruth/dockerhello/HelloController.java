20a6ceace27ee30f5c9dd7d0cea6361d38be607c
Petru <petre.hertea@gmail.com>
2025-02-12 23:43:15 +0200

    Spring Dockerized Hello World!

---
+++ dockerhello/src/main/java/com/petruth/dockerhello/HelloController.java
@@ -0,0 +1,12 @@
+package com.petruth.dockerhello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String helloDocker(){
+        return "Hello, Docker World!";
+    }
+}
---
+++ dockerhello/src/test/java/com/petruth/dockerhello/DockerhelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petruth.dockerhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerhelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
