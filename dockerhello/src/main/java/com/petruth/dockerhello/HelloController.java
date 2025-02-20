112adc674402ca01d890b3be03761bb9cdded656
Petru <petre.hertea@gmail.com>
2025-02-20 15:17:27 +0200

    added lotto functionality

--- dockerhello/src/main/java/com/petruth/dockerhello/HelloController.java
+++ dockerhello/src/main/java/com/petruth/dockerhello/HelloController.java
@@ -3,10 +3,28 @@ package com.petruth.dockerhello;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String helloDocker(){
         return "Hello, Docker World!";
+    }
+
+    @GetMapping("/lotto")
+    public String getLottoWinner(){
+        Random randomNumber = new Random();
+
+        StringBuilder lottoNumbers = new StringBuilder();
+
+        lottoNumbers.append("The numbers for this round are: ");
+
+        for (int i = 0; i < 5; i++) {
+            lottoNumbers.append(randomNumber.nextInt(1,49));
+            lottoNumbers.append(" ");
+        }
+
+        return lottoNumbers.toString();
     }
 }
